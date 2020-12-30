98bcba64fdd7aac78bb94f3d76bb94c3ee8a1158
Benjamin <1234hteimaj4321@gmail.com>
2020-12-30 09:13:45 -0700

    Assign times when start button is clicked

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,6 +1,12 @@
 <template>
     <h1>Timer</h1>
     <TimerStartButton v-on:start="timerStartHandler" :isStarted="isStarted" />
+    <p>{{ startTime.sec }}</p>
+    <p>{{ startTime.min }}</p>
+    <p>{{ startTime.hr }}</p>
+    <p>{{ startTime.day }}</p>
+    <p>{{ startTime.mth }}</p>
+    <p>{{ startTime.yr }}</p>
 </template>
 
 <script>
@@ -20,13 +26,21 @@ export default {
                 hr: null,
                 day: null,
                 mth: null,
-                year: null,
+                yr: null,
             },
         }
     },
     methods: {
         timerStartHandler() {
             this.isStarted = !this.isStarted
+
+            if (this.isStarted) {
+                this.startTime.sec = new Date().getSeconds()
+                this.startTime.min = new Date().getMinutes()
+                this.startTime.day = new Date().getDay()
+                this.startTime.mth = new Date().getMonth()
+                this.startTime.yr = new Date().getFullYear()
+            }
         },
     },
 }
