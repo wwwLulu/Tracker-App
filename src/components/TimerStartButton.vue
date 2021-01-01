64a0ed8fbcbeef4708a71fa6e35b4163f8011a0c
Benjamin <1234hteimaj4321@gmail.com>
2020-12-31 21:16:03 -0700

    Added flipper to start button

--- src/components/TimerStartButton.vue
+++ src/components/TimerStartButton.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="flipper-container" v-on:click="$emit('startStopTimer')">
+    <div
+        v-bind:class="{ active: isStarted }"
+        class="flip-container"
+        v-on:click="$emit('startStopTimer')"
+    >
         <div class="flipper">
-            <button class="btn btn-normal">
-                {{ isStarted ? 'Stop' : 'Start' }}
-            </button>
+            <div class="front">
+                <button class="btn btn-normal">Start</button>
+            </div>
+
+            <div class="back">
+                <button class="btn btn-normal">Stop</button>
+            </div>
         </div>
     </div>
 </template>
@@ -15,3 +23,55 @@ export default {
     },
 }
 </script>
+
+<style lang="scss" scoped>
+.flip-container {
+    perspective: 1000px;
+}
+.flip-container,
+.front,
+.back {
+    height: 100px;
+    width: 200px;
+}
+
+/* flip speed goes here */
+
+.flip-container {
+    perspective: 1000px;
+}
+/* flip the pane when hovered */
+.flip-container.active .flipper {
+    transform: rotateY(180deg);
+}
+
+/* flip speed goes here */
+.flipper {
+    transition: 0.6s;
+    transform-style: preserve-3d;
+
+    position: relative;
+}
+
+/* hide back of pane during swap */
+.front,
+.back {
+    backface-visibility: hidden;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+/* front pane, placed above back */
+.front {
+    z-index: 2;
+    /* for firefox 31 */
+    transform: rotateY(0deg);
+}
+
+/* back, initially hidden pane */
+.back {
+    transform: rotateY(180deg);
+}
+</style>
